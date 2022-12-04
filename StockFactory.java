6911743a3e5d7037bbfcd776af68d48fd623d289
Huanjia(Sam) Liang <huanjial@uci.edu>
2022-12-04 15:30:46 -0500

    Added HoldingStock.java and StockFactory files

---
+++ StockFactory.java
@@ -0,0 +1,15 @@
+/**
+ * StockFactory is responsible for making a Stock.
+ * It also updates the stock price randomly
+ */
+public class StockFactory {
+
+
+    public void generateServiceStock(){
+
+    }
+
+    public void randomlyUpdateStock(){
+
+    }
+}

b2b6ee595b3e6b8ca6e12c529955b9019ea2f39a
ReggieSean <yuxuhuang@ucdavis.edu>
2022-12-11 23:53:55 -0500

    AccountFiles

---
+++ Views/CheckingWindow.java
@@ -0,0 +1,24 @@
+package Views;
+
+import Personnel.*;
+
+import javax.swing.*;
+
+public class CheckingWindow extends JFrame {
+    boolean admin = false;
+    private JPanel panel1;
+    private JLabel name;
+    private JPanel LeftFields;
+    private JPanel RightFields;
+
+    private JTextField balance;
+
+    public CheckingWindow(Customer subject, boolean admin){
+        this.admin = admin;
+    }
+
+
+
+
+
+}

d9c14a2b8e1b338d93a1dee2b1d4dfd805e0a8ba
ReggieSean <yuxuhuang@ucdavis.edu>
2022-12-14 16:00:49 -0500

    Working Windows' UI, balance.java is a mess

--- Account/AccountSaving.java
+++ Account/AccountSaving.java
@@ -1,7 +1,6 @@
 package Account;
 
 public class AccountSaving extends Account {
-    private double interestRate;
     private Balance balance;
     private final double minimumBalanceToKeepSecurity = 2500.0;
     private final double highBalanceCap= 5000.0;
--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //FrameATM frameATM = new FrameATM();
-        //CheckingWindow test = new CheckingWindow(new Customer(),new AccountChecking("wasd",0,0,0));
+        CheckingWindow test = new CheckingWindow(new Customer(),new AccountChecking("wasd",0,0,0));
         //SavingWindow test = new SavingWindow(new Customer(),new AccountSaving("wasd"));
         //SecurityWindow test = new SecurityWindow(new Customer(),new AccountSecurity("wasd"));
 

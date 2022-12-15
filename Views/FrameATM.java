b4016e7aee992f99c860c7cbc3c2a0a53d7e6e41
Specter43 <zheyuyang43@gmail.com>
2022-12-14 22:50:41 -0500

    Link login and signup functionality

--- Views/FrameATM.java
+++ Views/FrameATM.java
@@ -1,10 +1,16 @@
 package Views;
 
 import Account.Account;
+import Bank.Bank;
+import Personnel.Customer;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FrameATM extends JFrame {
     private JFrame frame;
@@ -19,19 +25,36 @@ public class FrameATM extends JFrame {
 
     public FrameATM() {
         createWindow();
+        JFrame current = this;
         logInButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                ManagerInfoWindow managerInfoWindow = new ManagerInfoWindow();
-                setVisible(false);
+                if (usernameTextField.getText().equals("admin") && Arrays.equals(PINPasswordField.getPassword(), "admin".toCharArray())) {
+                    ManagerInfoWindow managerInfoWindow = new ManagerInfoWindow();
+                    setVisible(false);
+                } else {
+                    char[] passwordInput = PINPasswordField.getPassword();
+                    String password = Stream.of(passwordInput).map(String::new).collect(Collectors.joining());
+                    ArrayList<Object> login = Bank.login(usernameTextField.getText(), password);
+                    System.out.println(login);
+                    if ((Boolean) login.get(0)) {
+                        Customer customer = (Customer) login.get(1);
+                        SpecificCustomerWindow specificCustomerWindow = new SpecificCustomerWindow(current, "Customer", customer.getName());
+                    }
+                }
             }
         });
         signUpButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                System.out.println("SHIT");
+                char[] passwordInput = PINPasswordField.getPassword();
+                String password = Stream.of(passwordInput).map(String::new).collect(Collectors.joining());
+                ArrayList<Object> signupInput = Bank.signUp(usernameTextField.getText(), password);
+                if ((Boolean) signupInput.get(0)) {
+                    SpecificCustomerWindow specificCustomerWindow = new SpecificCustomerWindow(current, "Customer", ((Customer) signupInput.get(1)).getName());
+                }
             }
         });
     }
